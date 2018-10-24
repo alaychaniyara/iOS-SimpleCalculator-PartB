abf5143c9fbe0ebc2c1b25f292831e3ab75d04a4
Alay Chaniyara <alaychaniyara@gmail.com>
2018-10-23 23:18:55 -0400

    Initial Commit

---
+++ iOS-InClass01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-InClass01
+//
+//  Created by Alay Chaniyara on 10/23/18.
+//  Copyright © 2018 Alay Chaniyara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
