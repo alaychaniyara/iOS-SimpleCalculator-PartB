d077a3ae9fd005896ff15a8617b867d8177c7538
Alay Chaniyara <alaychaniyara@gmail.com>
2018-11-13 23:02:32 -0500

    iOS-Inclass 1 to understand view controller and getting and setting data into the view and creating constrainted layouts. This simple calculator app lets you understand this.

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iOS-InClass01' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iOS-InClass01
+pod 'Toast-Swift', '~> 4.0.0'
+  target 'iOS-InClass01Tests' do
+    inherit! :search_paths
+  
+  # Pods for testing
+  end
+
+  target 'iOS-InClass01UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- iOS-InClass01/ViewController.swift
+++ iOS-InClass01/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var results: UILabel!
+   
+    @IBOutlet weak var perform: UIButton!
+    //    var characters = ""
+//    var isNumeric: Bool {
+//      
+//        let nums: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+//        return Set(self.characters).isSubset(of: nums)
+//    }
+    @IBOutlet weak var getFuntion: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func performCalculation(_ sender: Any) {
+        let fno = Double( firstNumber.text!)
+        let sno = Double( secondNumber.text!)
+        if((firstNumber.text?.isEmpty)! || (secondNumber.text?.isEmpty)!)
+        {
+            
+            self.view.makeToast("Input Cannot be Empty")
+            results.text = "Input cannot be empty"
+        }
+        else if getFuntion.selectedSegmentIndex == 0
+        {  let sum =  fno! + sno!
+            
+            results.text = "Results : \(sum)"
+            
+        }
+        else if getFuntion.selectedSegmentIndex == 1
+        {
+            let minus =  fno! - sno!
+            
+            results.text = "Results : \(minus)"
+        }
+        else if getFuntion.selectedSegmentIndex == 2
+        {
+            let multiply =  fno! * sno!
+            
+            results.text = "Results : \(multiply)"
+        }
+        else if getFuntion.selectedSegmentIndex == 3
+        {
+            if(sno! == 0){
+                //results.text = "Divide by zero error"
+                let alertController = UIAlertController(title: "Error", message: "Divide by zero error", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Default Action"), style: .default, handler: nil))
+                self.present(alertController, animated: true)
+            }
+            else
+            {
+                let divide =  fno! / sno!
+                
+                results.text = "Results : \(divide)"
+            }
+        }
+    }
+    
+  
+   
+    @IBAction func Clear(_ sender: Any) {
+        results.text = "Results : 0.00"
+        firstNumber.text = ""
+        secondNumber.text = ""
+    }
 }
 
